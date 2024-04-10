c6f13377ee464778fedbcf20a895d29a4292d726
MD <88189681+A11yTime@users.noreply.github.com>
2024-04-10 09:26:45 -0700

    Update FoodItem.vue

--- src/components/FoodItem.vue
+++ src/components/FoodItem.vue
@@ -1,1 +1,30 @@
+<template>
+    <div>
+        <h2>
+            {{ foodName }}
+            <img src="../assets/fav.png"  v-show="isFavorite" />
+           
+        </h2>    
+        <p>{{ foodDesc }}</p>
+        <button @click="toggleFavorite">Favorite</button>
+        
+    </div>
+</template>
+<script>
 
+export default {
+    props: ["foodName", "foodDesc", "isFavorite"],
+    methods: {
+        toggleFavorite(){
+            this.$emit('toggle-favorite', this.foodName)
+        }
+    }
+}
+</script>
+
+<style>
+    img {
+        height: 1.5em;
+        float: right;
+    }
+</style> 
