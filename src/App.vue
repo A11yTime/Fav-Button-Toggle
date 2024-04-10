4d91bf460b5d7fada88427a38d03339d1213585d
MD <m.sohel.sw@gmail.com>
2024-04-10 09:51:04 -0700

    test commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Food</h1>
+  <div id="wrapper">
+    <food-item
+    v-for="x in foods"
+    :key="x.name"
+    :food-name="x.name"
+    :food-desc="x.desc"
+    :is-favorite="x.favorite"
+    @toggle-favorite="receiveEmit"
+     />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FoodItem from './components/FoodItem.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   FoodItem
+  },
+  data() {
+     return {
+      foods: [
+        { name: 'Apples', desc: 'Apples are a type of fruit that grow on trees.', favorite: true},
+        { name: 'Pizza', desc: 'Pizza has a bread base with tomato sauce, cheese, and toppings on top.', favorite: false},
+        { name: 'Rice', desc: 'Rice is a type of grain that people like to eat.', favorite: false},
+        { name: 'Fish', desc: 'Fish is an animal that lives in water.', favorite: true},
+        { name: 'Cake', desc: 'Cake is something sweet that tastes good.', favorite: false}
+      ]
+     };
+  },
+  methods: {
+    receiveEmit(foodId) {
+      let foundFood = this.foods.find(food => food.name === foodId);
+      foundFood.favorite = !foundFood.favorite
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #wrapper {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  #wrapper > div {
+    border: dashed black 1px;
+    flex-basis: 120px;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
 </style>
---
+++ src/components/FoodItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2>
+            {{ foodName }}
+            <img src="../assets/fav.png"  v-show="isFavorite" />
+           
+        </h2>    
+        <p>{{ foodDesc }}</p>
+        <button @click="toggleFavorite">Favorite</button>
+        
+    </div>
+</template>
+<script>
+
+export default {
+    props: ["foodName", "foodDesc", "isFavorite"],
+    emits: ['toggle-favorite'],
+    methods: {
+        toggleFavorite(){
+            this.$emit('toggle-favorite', this.foodName)
+        }
+    }
+}
+</script>
+
+<style>
+    img {
+        height: 1.5em;
+        float: right;
+    }
+</style> 
